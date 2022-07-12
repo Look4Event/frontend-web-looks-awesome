ed1f46d89254bd0b1bc5c9021cda34536ef519ae
JJerryJi <jerryji040506@ucla.edu>
2022-07-12 17:46:24 +0800

    'Jerry debugs createevent_button'

--- src/pages/HardFilter.js
+++ src/pages/HardFilter.js
@@ -2,9 +2,14 @@ import CardList from '../components/pages/HardFilter/CardList';
 
 function HardFilterPage() {
     // return <h1>This is the page where you set all the hard filters.</h1>;
+
+    function addEventHandler(){
+        //  pass the Event Data to backend server here
+    }
+
     return (
         <section>
-            <CardList />
+            <CardList addNewEvent={addEventHandler}/>
         </section>
     ); 
 }
