d4933595b7e8810479cddcabd72509b898967d4c
JJerryJi <jerryji040506@ucla.edu>
2022-07-13 20:12:54 +0800

    Jerry navigates the page after submitting the form

--- src/pages/HardFilter.js
+++ src/pages/HardFilter.js
@@ -1,16 +1,20 @@
+import {useNavigate} from 'react-router-dom'
+
 import CardList from "../components/pages/HardFilter/CardList";
 
 function HardFilterPage() {
   // return <h1>This is the page where you set all the hard filters.</h1>;
-
+  const navigate = useNavigate();
   function addEventHandler(eventData) {
-    fetch("http://127.0.0.1/eventData.json",{
+    fetch("http://127.0.0.1/eventData.json",{ // the function is correct if the link is replaced by a correct link in Firebase -- Jerry
         method:'POST',
         body:JSON.stringify(eventData),
         headers:{
             'Content-Type':'application/json'
         }
-    });
+    }).then(()=>{
+      navigate('/results', {replace:true}) // return to result page 
+    })
   }
 
   return (
