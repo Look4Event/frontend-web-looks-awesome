1c49345e03888a8beabf1147236a19a9c2fa6564
justinchencya <justinchenapp@icloud.com>
2022-07-20 22:50:25 -0700

    Restored App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 
 import HomePage from './pages/Home';
 import HardFilterPage from './pages/HardFilter';
 import EventDisplayGuestPage from './pages/EventDisplayGuest';
 import FormLayout from './components/layouts/FormLayout';
-import CreateAccountPage from './pages/CreateAccount';
 
 function App() {
   return (
     <div>
       <FormLayout>
         <Routes>
-          <Route exact path='/' element={<HomePage />} />
-          <Route exact path='/create' element={<HardFilterPage />} />
-          <Route exact path='/results' element={<EventDisplayGuestPage />} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/create' element={<HardFilterPage />} />
+          <Route path='/results' element={<EventDisplayGuestPage />} />
         </Routes>
       </FormLayout>
     </div>
-  ); 
+  );
 }
 
 export default App;
