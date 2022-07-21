857fb5e8aca9d733c0bfae7339796c6724d257c9
justinchencya <justinchenapp@icloud.com>
2022-07-20 22:51:01 -0700

    Restored App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/Home';
 import HardFilterPage from './pages/HardFilter';
