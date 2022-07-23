f55cff1ed80c14e90e943fb3d1cb3838eb1526df
justinchencya <justinchenapp@icloud.com>
2022-07-23 13:19:02 -0700

    Added item select/deselect

--- src/components/items/EditShareItem.js
+++ src/components/items/EditShareItem.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import classes from './EditShareItem.module.css'
 import EditShareItemCard from './EditShareItemCard';
 
 function EditShareItem(props) {
+    const [itemActive, setItemActive] = useState(true);
+
+    function itemClickHandler() {
+        if (itemActive) {
+            setItemActive(false);
+        } else {
+            setItemActive(true);
+        }
+    }
+
     return (
-        <li className={classes.item}>
+        <li className={classes.item} onClick={itemClickHandler}>
             <EditShareItemCard>
-                <div className={classes.content}>
+                <div className={classes.content} style={{backgroundColor:itemActive ? 'transparent' : 'grey', opacity: itemActive ? 1 : 0.5}}>
                     <h1>{props.title}</h1>
                 </div>
             </EditShareItemCard>
