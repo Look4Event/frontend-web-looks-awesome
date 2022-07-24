2dbf5ab8a3ce859ed0e067e32746b03dd763b586
Fred Hu <huyuqi0514@126.com>
2022-07-24 03:29:34 -0700

    google auth on chrome

--- src/components/layouts/FormMainNavigation.js
+++ src/components/layouts/FormMainNavigation.js
@@ -1,8 +1,36 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import jwt_decode from "jwt-decode";
 
 import classes from './FormMainNavigation.module.css';
 
 function FormMainNavigation() {
+    const [ user, setUser ] = useState(null);
+
+    function handleCallbackResponse(response) {
+        var userObject = jwt_decode(response.credential);
+        setUser(userObject);
+    }
+
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: "514076665065-nrfu2lnrge91nos8h8h8l12kkbtfk0o2.apps.googleusercontent.com",
+            callback: handleCallbackResponse
+        });
+
+        google.accounts.id.renderButton(
+            document.getElementById("signInDiv"),
+            // https://developers.google.com/identity/gsi/web/reference/html-reference
+            {
+                theme: "filled_black",
+                size: "large",
+                type: "standard",
+                text: "continue_with",
+                shape: "pill",
+            }
+        );
+    }, []);
     return (
         <header className={classes.header}>
             <div>
@@ -10,9 +38,17 @@ function FormMainNavigation() {
                     LOOK4EVENT
                 </span>
                 <span className={classes.logo_right}>
-                    <button className={classes.login}>
-                        Sign In
-                    </button>
+                    { !user && (
+                            <div id="signInDiv"></div>
+                        )
+                    }
+                    { user && (
+                            <div>
+                                <img src={user.picture} className={classes.profile_img} alt="profile" />
+                                <span className={classes.welcome_words}>Hi, {user.given_name}!</span>
+                            </div>
+                        )
+                    }
                 </span>
             </div>
             <nav>
@@ -29,7 +65,6 @@ function FormMainNavigation() {
                     </li>
                 </ul>
             </nav>
-            
         </header>
     );
     
