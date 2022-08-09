ba13dc910c8183bdad348fa7692d77fa0cda73cd
Flynn Sui <zsui@ucsd.edu>
2022-08-09 02:16:54 -0700

    Submit button modify

--- src/components/pages/ParticipantView/ParticipantLayout.js
+++ src/components/pages/ParticipantView/ParticipantLayout.js
@@ -2,6 +2,7 @@ import React from "react";
 import testData from "./testData";
 import OptionCard from "./OptionCard";
 import classes from "./ParticipantLayout.module.css";
+import {useState} from 'react';
 
 function createEntry(option) {
   return (
@@ -16,10 +17,17 @@ function createEntry(option) {
 
 function ParticipantLayout(props) {
   document.body.style.backgroundColor = "#BDE9FF";
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        console.log("Submitted");
+    };
+
   return (
     <div className={classes.layout}>
-      <form className={classes.OptionCard}>
+      <form className={classes.OptionCard} onSubmit={handleSubmit}>
         {testData.map(createEntry)}
+        TODO: Set the submit information
         <button type="submit" className={classes.submit}>
           Submit
         </button>
